9d78d987ff89e7a84933f6bfada7e4ddd59d10b4
Iulian Constantin <iulian.constantin@outlook.com>
2019-05-05 20:12:53 +0300

    unit merge

---
+++ UnitTestProject1/ProcessInfoTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Diagnostics;
+using ProcessList;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ProcessInfoTest
+    {
+        [TestMethod]
+        public void GetProcessesInfo()
+        {
+            //Arrange
+            Mock<Process> mockProc = new Mock<Process>();
+
+            var
+
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("517cc188-8fe2-40f4-87a9-840cef0acbf3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
